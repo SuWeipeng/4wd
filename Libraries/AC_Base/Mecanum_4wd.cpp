fc6b7d3bc51e3bc9873917b0acca3223c0537100
SuWeipeng <weipeng.su@outlook.com>
2019-08-23 15:33:00 +0800

    与 ardupilot 一样，采用 C++11标准起支持的对象初始化方式：T object { arg }; (C++11 起)
    参考链接如下
    https://zh.cppreference.com/w/cpp/language/direct_initialization

--- Libraries/AC_Base/Mecanum_4wd.cpp
+++ Libraries/AC_Base/Mecanum_4wd.cpp
@@ -1,18 +1,8 @@
-#include <stm32f4xx_hal.h>
-#include <AP_Motors.h>
-#include <AC_PID.h>
 #include <vectorN.h>
 #include "Mecanum_4wd.h"
 #include "usb_device.h"
 
 typedef VectorN<float,3> Vector3f;
-
-extern TIM_HandleTypeDef htim1;
-extern TIM_HandleTypeDef htim2;
-extern TIM_HandleTypeDef htim3;
-extern TIM_HandleTypeDef htim4;
-extern TIM_HandleTypeDef htim5;
-extern TIM_HandleTypeDef htim12;
 
 static float r1[3] = { 1,  1,  (HALF_BASE_LENGTH_M+HALF_BASE_WIDTH_M)};
 static float r2[3] = { 1, -1, -(HALF_BASE_LENGTH_M+HALF_BASE_WIDTH_M)};
@@ -26,96 +16,13 @@ Mecanum_4wd::Mecanum_4wd()
 , _motor2_fl_rpm(0)
 , _motor3_bl_rpm(0)
 , _motor4_br_rpm(0)
-{
-  _pid_1 = new AC_PID(1.2f, 
-                      1.2f, 
-                      0.0f, 
-                      0.0f,
-                      95.0f,
-                      1.0f,
-                      1.0f,
-                      0.85f,
-                      0.02f);
-  _pid_2 = new AC_PID(1.2f, 
-                      1.2f, 
-                      0.0f, 
-                      0.0f,
-                      95.0f,
-                      1.0f,
-                      1.0f,
-                      0.85f,
-                      0.02f);
-  _pid_3 = new AC_PID(1.2f, 
-                      1.2f, 
-                      0.0f, 
-                      0.0f,
-                      95.0f,
-                      1.0f,
-                      1.0f,
-                      0.85f,
-                      0.02f);
-  _pid_4 = new AC_PID(1.2f, 
-                      1.2f, 
-                      0.0f, 
-                      0.0f,
-                      95.0f,
-                      1.0f,
-                      1.0f,
-                      0.85f,
-                      0.02f);
-  
-  _motor1_fr = new AP_Motors(&htim1,
-                             -1,
-                             GPIOD,
-                             GPIO_PIN_10,
-                             GPIO_PIN_8,
-                             &htim12,
-                             TIM_CHANNEL_1,
-                             99,
-                             _pid_1
-                               );
-  _motor2_fl = new AP_Motors(&htim5,
-                             -1,
-                             GPIOE,
-                             GPIO_PIN_14,
-                             GPIO_PIN_12,
-                             &htim2,
-                             TIM_CHANNEL_3,
-                             99,
-                             _pid_2
-                               );
-  _motor3_bl = new AP_Motors(&htim3,
-                             -1,
-                             GPIOE,
-                             GPIO_PIN_15,
-                             GPIO_PIN_13,
-                             &htim2,
-                             TIM_CHANNEL_4,
-                             99,
-                             _pid_3
-                               );
-  _motor4_br = new AP_Motors(&htim4,
-                             -1,
-                             GPIOD,
-                             GPIO_PIN_11,
-                             GPIO_PIN_9,
-                             &htim12,
-                             TIM_CHANNEL_2,
-                             99,
-                             _pid_4
-                               ); 
-}
+{}
 
 Mecanum_4wd::~Mecanum_4wd()
-{
-  delete _motor1_fr;
-  delete _motor2_fl;
-  delete _motor3_bl;
-  delete _motor4_br;
-}
+{}
 
 void Mecanum_4wd::vel2rpm(float& vel_x, float& vel_y, float& rad_z)
-{  
+{
   float scale = 1;
   float vel[3] = {vel_x * 60, vel_y * 60, rad_z * 60}; // m/s -> m/min, rad/s -> rad/min
   Vector3f _vel(vel);
@@ -143,16 +50,16 @@ void Mecanum_4wd::vel2rpm(float& vel_x, float& vel_y, float& rad_z)
 
 void Mecanum_4wd::run()
 {
-  _motor1_fr->set_rpm(_motor1_fr_rpm);
-  _motor2_fl->set_rpm(_motor2_fl_rpm);
-  _motor3_bl->set_rpm(_motor3_bl_rpm);
-  _motor4_br->set_rpm(_motor4_br_rpm);
+  _motor1_fr.set_rpm(_motor1_fr_rpm);
+  _motor2_fl.set_rpm(_motor2_fl_rpm);
+  _motor3_bl.set_rpm(_motor3_bl_rpm);
+  _motor4_br.set_rpm(_motor4_br_rpm);
 }
 
 void Mecanum_4wd::stop()
 {
-  _motor1_fr->set_rpm(0);
-  _motor2_fl->set_rpm(0);
-  _motor3_bl->set_rpm(0);
-  _motor4_br->set_rpm(0);
+  _motor1_fr.set_rpm(0);
+  _motor2_fl.set_rpm(0);
+  _motor3_bl.set_rpm(0);
+  _motor4_br.set_rpm(0);
 }
--- Libraries/AC_Base/Mecanum_4wd.h
+++ Libraries/AC_Base/Mecanum_4wd.h
@@ -1,14 +1,22 @@
 #ifndef __MECANUM_4WD_H__
 #define __MECANUM_4WD_H__
 
+#include <stm32f4xx_hal.h>
+#include <AP_Motors.h>
+#include <AC_PID.h>
+
 #define HALF_BASE_LENGTH_M  0.075f
 #define HALF_BASE_WIDTH_M   0.070f
 #define WHEEL_DIAMETER_M    0.096f   
 #define WHEEL_RADIUS_M      (WHEEL_DIAMETER_M / 2.0f)
 #define MECANUM_VCP_DEBUG   0
 
-class AP_Motors;
-class AC_PID;
+extern TIM_HandleTypeDef htim1;
+extern TIM_HandleTypeDef htim2;
+extern TIM_HandleTypeDef htim3;
+extern TIM_HandleTypeDef htim4;
+extern TIM_HandleTypeDef htim5;
+extern TIM_HandleTypeDef htim12;
 
 class Mecanum_4wd
 {
@@ -21,8 +29,14 @@ public:
   void vel2rpm(float& vel_x, float& vel_y, float& rad_z);
 
 private:
-  AP_Motors *_motor1_fr, *_motor2_fl, *_motor3_bl, *_motor4_br;
-  AC_PID    *_pid_1, *_pid_2, *_pid_3, *_pid_4;
+  AP_Motors _motor1_fr{&htim1, -1, GPIOD, GPIO_PIN_10, GPIO_PIN_8 , &htim12, TIM_CHANNEL_1, 99, &_pid_1};
+  AP_Motors _motor2_fl{&htim5, -1, GPIOE, GPIO_PIN_14, GPIO_PIN_12, &htim2 , TIM_CHANNEL_3, 99, &_pid_2};
+  AP_Motors _motor3_bl{&htim3, -1, GPIOE, GPIO_PIN_15, GPIO_PIN_13, &htim2 , TIM_CHANNEL_4, 99, &_pid_3};
+  AP_Motors _motor4_br{&htim4, -1, GPIOD, GPIO_PIN_11, GPIO_PIN_9 , &htim12, TIM_CHANNEL_2, 99, &_pid_4};
+  AC_PID    _pid_1{1.2f, 1.2f, 0.0f, 0.0f, 95.0f, 1.0f, 1.0f, 0.85f, 0.02f};
+  AC_PID    _pid_2{1.2f, 1.2f, 0.0f, 0.0f, 95.0f, 1.0f, 1.0f, 0.85f, 0.02f};
+  AC_PID    _pid_3{1.2f, 1.2f, 0.0f, 0.0f, 95.0f, 1.0f, 1.0f, 0.85f, 0.02f};
+  AC_PID    _pid_4{1.2f, 1.2f, 0.0f, 0.0f, 95.0f, 1.0f, 1.0f, 0.85f, 0.02f};
   
   float     _motor1_fr_rpm, _motor2_fl_rpm, _motor3_bl_rpm, _motor4_br_rpm;
 };
